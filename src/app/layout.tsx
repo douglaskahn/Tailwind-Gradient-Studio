13f3d9653ee186d85a3636885bcaa1fb94e46a61
Douglas Kahn <douglaskahn@gmail.com>
2025-09-23 19:32:14 +0000

    I have my own instructions on how to set up Google Analytics. Install ma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Inter, Roboto_Flex } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -27,6 +29,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6QF6W90VTR"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6QF6W90VTR');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning className={cn("font-body antialiased", inter.variable, robotoFlex.variable)}>
         {children}
         <Toaster />
